b515e6da359834e1321cb225292f9c8db4661d81
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-12-20 20:17:44 +0800

    Set font styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cGray-100": "#FFFFFF",
+        "cGray-200": "#EEEEEE",
+        "cGray-300": "#B4B4B4",
+        "cGray-400": "#7D7D7D",
+        "cGray-500": "#333333",
+      },
+    },
   },
   plugins: [],
 };
