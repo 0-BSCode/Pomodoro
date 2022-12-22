a32fbafa8a037078f3f4ac310c469f7109b91fcd
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-12-22 11:41:54 +0800

    Modified path aliases

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import Header from "@/components/_common/header";
+import Header from "@components/_common/header";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
