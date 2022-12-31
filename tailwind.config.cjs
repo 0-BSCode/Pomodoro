eb55ab65384c958d79b9745b3342a027eb3b4294
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-12-31 15:09:31 +0800

    Custom screen size breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "375px",
+      sm: "480px",
+      md: "512px",
+      lg: "768px",
+      xl: "1024px",
+      "2xl": "1280px",
+      "3xl": "1440px",
+    },
     extend: {
       colors: {
         "cGray-100": "#FFFFFF",
